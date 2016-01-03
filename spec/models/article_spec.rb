73670fb07ea6e6822da271fc034da0fa47df9720
Christian Franco <cfs.bsi@outlook.com>
2016-01-03 18:10:49 +0000

    Create Article Integration test

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	it "must have a title" do
+		article = Article.new()
+		expect(article.valid?).to be_falsey
+	end
 end
