91f394858c674d1688dc9e61ad6c217c06a8eff9
Christian Franco <cfs.bsi@outlook.com>
2016-01-07 22:50:51 +0000

    Implement Sign out for users

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,15 +1,15 @@
 module ArticlesHelper
-	def devise_error_messages!
-	 return '' if resources.errors.empty?
+# 	def devise_error_messages!
+# 	 return '' if resources.errors.empty?
 	 
-	 messages = resource.errors.full_messages.map {
-	 	 |msg| content_tag(:li, msg) }.join 
-	 html = <<-HTML
-<div class="alert alert-error alert-danger">
-<a htef='#' class='close' data-dismiss='alert' &#215;></a>
-<%= content_tag :div, msg if msg.is_a?(String) %>
-</div>
-HTML
- 		html.html_safe
-	end
+# 	 messages = resource.errors.full_messages.map {
+# 	 	 |msg| content_tag(:li, msg) }.join 
+# 	 html = <<-HTML
+# <div class="alert alert-error alert-danger">
+# <a htef='#' class='close' data-dismiss='alert' &#215;></a>
+# <%= content_tag :div, msg if msg.is_a?(String) %>
+# </div>
+# HTML
+# 		html.html_safe
+# 	end
 end
